818385eb4ddb7bf5541310c732d98edcca2d7eca
RubixQber <joshua.lowe@menloschool.org>
2021-03-16 22:49:18 -0700

    commented

--- working_shell.py
+++ working_shell.py
@@ -1,17 +1,24 @@
-import os
-import subprocess
-import fnmatch
-import glob
-import shlex
-import subprocess
-import signal
-import getpass
-import socket
+import os              # used for general interactions with the operating system
+import subprocess      # used for creating subprocesses in the shell
+import fnmatch         # not used???
+import glob            # used to handle glob notation
+import shlex           # used to split input in the shell
+import signal          # used for sending signals to subprocesses
+import getpass         # used *exclusively* to get the current user's name
+import socket          # used *exclusively* to get the current hostname
+
+"""
+Linux Shell in Python
+Contributors: Andrew Tung, Katherine Tung, Joshua Lowe
+March 16, 2021
+"""
 
 jobs = []
-USER = getpass.getuser()
-HOME = os.getenv("HOME")
-HOST = socket.gethostname()
+USER = getpass.getuser()    # gets the username
+HOME = os.getenv("HOME")    # gets the home directory
+HOST = socket.gethostname() # gets the hostname
+
+# changes directory
 def sh_cd(inp):
     try:
         args=shlex.split(inp)
@@ -26,11 +33,11 @@ def sh_cd(inp):
     except Exception:
             print("cd: no such file or directory:")
 
-
+# sends the interrupt signal to a target process
 def signal_handler(process):
     process.send_signal(signal.SIGINT)
 
-
+# loops infinitely until exit command, executing commands
 def main():
     current_process = 0
     while True:
@@ -41,10 +48,11 @@ def main():
                 break
             elif not inp:
                 pass
-            elif "sudo " in inp:
+            elif "sudo " in inp:  # we don't like troll cases or security holes
                 print("hey wait no you can't do that")
             elif "cd " in inp:
                 sh_cd(inp)
+            # if builtin jobs called, loops through jobs and checks all of them
             elif "jobs" in inp:
                 to_ret = ""
                 rem = []
@@ -57,7 +65,8 @@ def main():
                 for job in rem:
                     jobs.remove(job)
                 print(to_ret)
-            elif "fg" in inp:
+
+            elif "fg" in inp: # restarts a background job in the foreground
                 check_jobs()
                 id = inp[inp.index(" ") + 1:]
                 found = False
@@ -65,12 +74,12 @@ def main():
                     if int(id) == job.pid:
                         job.send_signal(signal.SIGSTOP)
                         p = subprocess.Popen(job.args, shell=True)
-                        p.communicate()
+                        p.wait()
                         found = True
                 if not found:
                     print("That job doesn't exist")
 
-            elif "bg" in inp:
+            elif "bg" in inp: # restarts a background job
                 check_jobs()
                 id = inp[inp.index(" ") + 1:]
                 found = False
@@ -81,16 +90,19 @@ def main():
                         found = True
                 if not found:
                     print("That job doesn't exist")
-            elif inp[-1] == "&":
+
+            elif inp[-1] == "&": # starts a process in the background
                 p = subprocess.Popen(inp[:-1], shell=True, start_new_session=True)
                 jobs.append(p)
                 print("[" + str(jobs.index(p) + 1) + "] " + str(p.pid))
-            else:
+
+            else: # starts a process
                 p = subprocess.Popen(inp, shell=True)
                 current_process = p
                 p.wait()
             reap()
 
+        # intercepts ctrl-c command and stops process rather than python shell
         except KeyboardInterrupt:
             if current_process:
                 signal_handler(current_process)
@@ -99,6 +111,7 @@ def main():
             else:
                 print()
 
+# reaps dead children at end of execution loop
 def reap():
     rem = []
     indices = []
@@ -114,6 +127,7 @@ def reap():
         for index, i in enumerate(rem):
             print("[" + str(indices[index] + 1) + "] Done     " + str(i.args) + "&")
 
+# checks jobs to see whether done or running
 def check_jobs():
     rem = []
     for job in jobs:
